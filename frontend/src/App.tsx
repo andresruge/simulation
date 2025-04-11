d6a6cfc8098659a649454a4e8e0f9b2e48a86462
Andrés Ruge <andres_ruge@hotmail.com>
2025-04-10 21:38:30 -0400

    Fix text alignment issues

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProcessListPage from "./pages/ProcessListPage";
 import ProcessDetailPage from "./pages/ProcessDetailPage";
+import "./App.css";
 
 function App() {
   return (
